de00bbad7ef7b41adcf7b2c2f4faf45c8b7ff10d
sivirk <sivirk@gmail.com>
2014-03-05 17:45:01 +0900

    templates

--- apps/web/src/routes.erl
+++ apps/web/src/routes.erl
@@ -10,8 +10,8 @@ init(State, Ctx) ->
     Module = route_prefix(Path),
     {ok, State, Ctx#context{path=Path,module=Module}}.
 
-% route_prefix(<<"/ws/",P/binary>>) -> route(P);
-% route_prefix(<<"/",P/binary>>) -> route(P);
+route_prefix(<<"/ws/",P/binary>>) -> route(P);
+route_prefix(<<"/",P/binary>>) -> route(P);
 route_prefix(P) -> route(P).
 
 % route(<<>>)              -> login;
